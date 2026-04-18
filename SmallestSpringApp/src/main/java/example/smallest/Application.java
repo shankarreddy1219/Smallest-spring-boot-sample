ef600a2c4e277e38dd627b16b2c2dace329f1193
shankarreddy1219 <86099586+shankarreddy1219@users.noreply.github.com>
2026-04-18 09:52:50 +0530

    Update Application.java

--- SmallestSpringApp/src/main/java/example/smallest/Application.java
+++ SmallestSpringApp/src/main/java/example/smallest/Application.java
@@ -1,15 +1,14 @@
 package example.smallest;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer; // ✅ fixed
 
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-    	 SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
+
 }
