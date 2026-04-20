fedc19af32f0f044345288b75b4ebca65cffee98
shankarreddy1219 <86099586+shankarreddy1219@users.noreply.github.com>
2026-04-20 12:42:25 +0530

    Update sonar.sh

--- SmallestSpringApp/sonar.sh
+++ SmallestSpringApp/sonar.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-exec sonar-scanner \
-  -Dsonar.projectKey=shankarreddy1219_Smallest-spring-boot-sample \
+sonar-scanner \
+  -Dsonar.projectKey=Smallest-spring-boot-sample \
   -Dsonar.organization=shankarreddy1219 \
   -Dsonar.host.url=https://sonarcloud.io \
   -Dsonar.login="$SONAR_TOKEN" \
   -Dsonar.sources=/harness/SmallestSpringApp/src
+exit 0
